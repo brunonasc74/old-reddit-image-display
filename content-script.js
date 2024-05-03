a33f2e08716a585d58a244179beff1e59cc1da6c
brunonasc74 <bruno.hpn74@gmail.com>
2024-05-03 02:22:42 -0300

    fix: Image max height to 600px

--- content-script.js
+++ content-script.js
@@ -2,7 +2,7 @@ const imgLinks = document.querySelectorAll('a');
 
 imgLinks.forEach((imgLink) => {
   if (imgLink.innerHTML === '&lt;image&gt;') {
-    const imageTag = `<img src="${imgLink.href}" alt="Image" max-width=300 max-height=300 />`;
+    const imageTag = `<img class="ig-s" src="${imgLink.href}" alt="Image" />`;
     imgLink.innerHTML = imageTag;
   }
 });
